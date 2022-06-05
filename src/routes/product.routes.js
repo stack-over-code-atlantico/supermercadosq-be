43f0beb21810f4fd79365b2a6e3b38412a027365
MarcelleTabosa <marcelle.tabosa.97@gmail.com>
2022-06-05 20:41:10 -0300

    feat/BE-05: Added product route

---
+++ src/routes/product.routes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const route = express();
+const productRepository = require('../modules/entities/Produto/repository/produtoRepository');
+
+route.get('/product-read', async (req, res) => {
+    const product = await productRepository.productRead();
+    return res.json(product);
+});
+
+route.post('/product-create', async (req, res) => {
+  const {
+    nome,
+    ingredientes,
+    imagem,
+    feedbacks_produtos,
+    id_usuario
+  } = req.body;
+  const product = await productRepository.productCreate(
+    nome,
+    ingredientes,
+    imagem,
+    feedbacks_produtos,
+    id_usuario
+  );
+  res.status(201).json(product);
+});
+
+route.put('/:id_product', async (req, res) => {
+  const { id_produto } = req.params;
+  const {
+    nome,
+    ingredientes,
+    imagem,
+    feedbacks_produtos,
+    id_usuario
+  } = req.body;
+  const product = await productRepository.productUpdate(
+    Number(id_produto),
+    nome,
+    ingredientes,
+    imagem,
+    feedbacks_produtos,
+    id_usuario
+  );
+  return res.status(204).json(product);
+});
+
+route.delete('/:id_produto', async (req, res) => {
+  const { id_produto } = req.params;
+  const product = await productRepository.productDelete(Number(id_produto));
+  return res.status(204).send(product);
+});
+
+module.exports = route;
