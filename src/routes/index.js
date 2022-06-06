c150ef7762ba67b8473d6c7caaef9448be1b2331
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2022-06-06 19:54:42 -0300

    BE/Feat - Atualizando o index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,17 @@
 const express = require('express');
-const app = express();
 const userRoute = require('./users.routes');
 const addressRoute = require('./address.routes');
-const loginRoute = require('./login.routes');
-const loginRoute = require('./product.routes');
+const loginRoute = require('./login.routes')
+const errors = require('@Middleware/errors')
+
+const app = express();
 
 app.use(express.json());
 
 app.use('/users', userRoute);
 app.use('/endereco', addressRoute);
 app.use('/login', loginRoute);
-app.use('/product', productRoute);
 
-app.use( (error, req, res, next) => {
-  if (error instanceof Error) {
-      return res.status(404).json({
-          status: 'error',
-          message: `${error.message}`,
-      });
-  }
-
-  return res.status(500).json({
-      status: 'error',
-      message: `Internal server error - ${error.message}`,
-  })
-  //next(error);
-});
+app.use(errors);
 
 module.exports = app;
