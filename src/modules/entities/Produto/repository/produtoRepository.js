cc38ee28869a8bca9df9e223cf53e0842f16d556
Victor Elias <svvictorelias@gmail.com>
2022-06-13 23:00:56 -0300

    refatorando produtos

--- src/modules/entities/Produto/repository/produtoRepository.js
+++ src/modules/entities/Produto/repository/produtoRepository.js
@@ -1,122 +1,53 @@
-const prisma = require("../../../../database/prismaClient");
+const prisma = require('../../../../database/prismaClient');
+const { produto } = require('../../../../database/prismaClient');
 
-const findUniqueProduct = async (id_produto) => {
-	const result = await prisma.produto.findUnique({
-		where: {
-			id_produto
-		},
-	});
-
-    if(!result)
-    console.log('Product does not exist');
-
-    else
-    console.log(result)
-
-	return result;
+const findUniqueProduto = async id_produto => {
+  const result = await prisma.produto.findFirst({
+    where: { id_produto }
+  });
+  return result;
 };
 
-const productRead = async () => {
-	const result = await prisma.produto.findMany({
-        where: {
-            OR: [
-                {
-                    status: {
-                        equals: null,
-                    },
-                },
-                {
-                    status: {
-                        equals: "true",
-                    },
-                },
-              ],
-            
-          },
-    });
-	return result;
+const produtosRead = async () => {
+  const result = await prisma.produto.findMany({});
+  return result;
 };
 
-const productCreate = async (
-    nome,
-    ingredientes,
-    imagem,
-    id_usuario
-) => {
-	const result = await prisma.produto.create({
-		data: {
-            nome: nome,              
-            ingredientes: ingredientes,  
-            imagem: imagem,            
-            data_postagem: new Date(),
-            status: null,          
-            feedbacks_produtos: 0,
-            id_usuario: id_usuario,  
-            editado: false,       
-            id_admin_relator: null       
-		},
-	});
-	return result;
-};
-
-const productUpdate = async (
-    id_produto,     
-    nome,              
-    ingredientes,      
-    imagem,       
-    feedbacks_produtos,
-    id_usuario      
-) => {
-    const product = await prisma.produto.findFirst({
-        where: {id_produto},
-    });
-	const result = await prisma.produto.update({
-        where: {id_produto},
-		data: {
-            nome: nome,              
-            ingredientes: ingredientes,  
-            imagem: imagem,            
-            data_postagem: product.data_postagem,
-            status: product.status,          
-            feedbacks_produtos: feedbacks_produtos,
-            editado: true,       
-            id_admin_relator: product.id_admin_relator        
-		},
-	});
-    return result;
-};
-
-const productDelete = async (id_produto) => {
-    try{
-        const result = await prisma.produto.delete({
-            where: {
-                id_produto,
-            },
-        });
-        console.log('Product removed successfully. Below is the product information.');
-        console.log(result);
-        return result;
-    } catch (e){
-        console.log('Product does not exist');
+const produtosCreate = async (nome, ingredientes, imagem, id_usuario) => {
+  const result = await prisma.produto.create({
+    data: {
+      nome,
+      ingredientes,
+      imagem,
+      status: null,
+      data_postagem: new Date(),
+      feedbacks_produtos: 0,
+      id_usuario,
+      editado: false,
+      id_admin_relator: null
     }
+  });
+  return result;
 };
 
-// productUpdate (
-//     10,
-//     'Barra de Cereal Morango com Chocolate e Canela e limão',
-//     'INGREDIENTES: CEREAIS (AVEIA E FLOCOS DE CEREAIS), GLICOSE DE MILHO, COBERTURA SABOR CHOCOLATE, AÇÚCAR INVERTIDO, GORDURA VEGETAL, POLPA DE MORANGO, CASSIS, CENOURA, CORANTE BETACAROTENO, ANTIOXIDANTE LECITINA DE SOJA, ACIDULANTES ÁCIDO CÍTRICO E LÁCTICO E AROMATIZANTE. CONTÉM GLÚTEN. CONTÉM LACTOSE. ALÉRGICOS: CONTÉM AVEIA, LEITE E DERIVADOS DE CEVADA E DE SOJA. PODE CONTER AMÊNDOA, AMENDOIM, AVELÃ, CASTANHA-DE-CAJU, CASTANHA-DO-BRASIL, CENTEIO, LÁTEX NATURAL, MACADÂMIA, NOZES, PECÃS, PISTACHE E DERIVADOS DE TRIGO.',
-//     null,
-//     null,
-//     13,
-// );
-// productRead();
-// findUniqueProduct(10);
-// productDelete(9);
+const produtosUpdate = async (id_produto, nome, ingredientes, imagem) => {
+  const produto = await prisma.produto.findFirst({
+    where: { id_produto }
+  });
+  const result = await prisma.produto.update({
+    where: { id_produto },
+    data: {
+      nome: nome ? nome : produto.nome,
+      ingredientes: ingredientes ? ingredientes : produto.ingredientes,
+      imagem: imagem ? nome : produto.imagem
+    }
+  });
+  return result;
+};
 
 module.exports = {
-	findUniqueProduct,
-	productRead,
-    productCreate,
-    productDelete,
-    productUpdate,
+  findUniqueProduto,
+  produtosRead,
+  produtosCreate,
+  produtosUpdate
 };
