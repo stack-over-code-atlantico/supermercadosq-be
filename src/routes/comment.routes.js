8cbef4a37239b9f66febb19359f06ec8255e99b2
Helen Cris <helencrisfernandes@hotmail.com>
2022-06-15 19:56:20 -0300

    Feat: delete comment

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -14,10 +14,5 @@ route.put('/:id_comentario', comentarioController.update);
 
 route.put('/:id_comentario/delete', comentarioController.delete);
 
-route.delete('/:id_comentario', async (req, res) => {
-  const { id_comentario } = req.params;
-  const comentario = await comentarioRepositorio.comentarioDelete(Number(id_comentario));
-  return res.status(204).send(comentario);
-});
 
 module.exports = route;
