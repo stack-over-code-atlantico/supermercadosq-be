38462e8e4cc9fa025dcb5ff0fbadfe2baf415433
Victor Elias <svvictorelias@gmail.com>
2022-06-17 17:31:31 -0300

    FIX refatorando Login

--- src/modules/entities/Login/controller/LoginController.js
+++ src/modules/entities/Login/controller/LoginController.js
@@ -4,10 +4,8 @@ const loginService = new LoginService();
 
 class LoginController {
   async signin(req, res) {
-    const { id_usuario } = req
-    const { ativo } = req.params;
     const { email, senha } = req.body;
-    const login = await loginService.signin(id_usuario, ativo, email, senha);
+    const login = await loginService.signin(email, senha);
 
     if(login instanceof Error) {
       return res.status(401).json(login.message);
