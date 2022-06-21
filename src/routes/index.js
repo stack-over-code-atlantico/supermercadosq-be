fb50f506ad2ca92d70eb6a57351408c3c7890697
Victor Elias <svvictorelias@gmail.com>
2022-06-21 11:55:00 -0300

    Fix alterado cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const userRoute = require('./users.routes');
-const loginRoute = require('./login.routes')
-const errors = require('@Middleware/errors')
+const loginRoute = require('./login.routes');
+const errors = require('@Middleware/errors');
 const comentarioRoute = require('./comment.routes');
 const produtoRoute = require('./product.routes');
 
 const app = express();
-app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
 app.use(express.json());
 app.use('/users', userRoute);
 app.use('/login', loginRoute);
